6444fd4249bd19c18a118b2e6d3596c5c491e694
Dan Meliza <dan@meliza.org>
2022-09-28 20:38:19 -0400

    add numpy include

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
 from setuptools import setup, Extension
+import numpy
 
 setup(ext_modules=[Extension("quickspikes.spikes", sources=["quickspikes/spikes.pyx"]),
-                   Extension("quickspikes._chebyshev", sources=["quickspikes/chebyshev.c"])])
+                   Extension("quickspikes._chebyshev", sources=["quickspikes/chebyshev.c"])],
+      include_dirs=[numpy.get_include()])

66822e0fd8b77ffdb9352a505564778ae7cbc800
Dan Meliza <dan@meliza.org>
2024-09-27 19:58:30 -0400

    turn on wheel building; bump version

--- quickspikes/__init__.py
+++ quickspikes/__init__.py
@@ -1,11 +1,11 @@
 # -*- mode: python -*-
 """spike detection routines
 
-Copyright (C) 2013 Dan Meliza <dmeliza@gmail.com>
+Copyright (C) 2013-2024 Dan Meliza <dmeliza@gmail.com>
 Created Wed Jul 24 09:26:36 2013
 """
 from .spikes import detector, peaks, subthreshold
 from .tools import filter_times, realign_spikes
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 __all__ = ["detector", "peaks", "subthreshold", "realign_spikes", "filter_times"]

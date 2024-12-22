d98530e56937cc55d85e2ffb70736b53259d1c44
Dan Meliza <dan@meliza.org>
2024-12-22 09:25:01 -0500

    trying out meson

--- quickspikes/__init__.py
+++ quickspikes/__init__.py
@@ -7,5 +7,14 @@ Created Wed Jul 24 09:26:36 2013
 from .spikes import detector, peaks, subthreshold
 from .tools import filter_times, realign_spikes
 
-__version__ = "2.0.5"
+try:
+    from importlib.metadata import version
+    __version__ = version("quickspikes")
+except ImportError:
+    # For Python < 3.8
+    from importlib_metadata import version
+    __version__ = version("quickspikes")
+except Exception:
+    # If package is not installed (e.g. during development)
+    __version__ = "unknown"
 __all__ = ["detector", "peaks", "subthreshold", "realign_spikes", "filter_times"]
--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- mode: python -*-
-import numpy
-from setuptools import Extension, setup
+from setuptools import setup
 
-setup(
-    ext_modules=[Extension("quickspikes.spikes", sources=["quickspikes/spikes.pyx"])],
-    include_dirs=[numpy.get_include()],
-)
+setup()
